5547f912af9556dd1db0f87df2ff6a7339cbb2fb
shuao23 <shuao23@gmail.com>
2018-08-22 22:44:01 -1000

    Added simple follow script

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Bool3 following;
+    [SerializeField]
+    private UpdateMode updateMode;
+
+    private Vector3 initialPosition;
+
+    void Awake()
+    {
+        initialPosition = transform.position;
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (ShouldUpdate(UpdateMode.FixedUpdate))
+        {
+            UpdateInternal();
+        }
+    }
+
+    private void Update()
+    {
+        if(ShouldUpdate(UpdateMode.Update))
+        {
+            UpdateInternal();
+        }
+    }
+
+
+    private bool ShouldUpdate(UpdateMode currentUpdate)
+    {
+        return (currentUpdate & updateMode) != 0;
+    }
+
+    private void UpdateInternal()
+    {
+        Vector3 newPosition = initialPosition;
+
+        if (following.x)
+        {
+            newPosition.x = target.position.x;
+        }
+
+        if (following.y)
+        {
+            newPosition.y = target.position.y;
+        }
+
+        if (following.z)
+        {
+            newPosition.z = target.position.z;
+        }
+
+        transform.position = newPosition;
+    }
+
+
+    [Serializable]
+    private struct Bool3
+    {
+        public bool x;
+        public bool y;
+        public bool z;
+    }
+
+    private enum UpdateMode
+    {
+        None = 0,
+        Update = 1,
+        FixedUpdate = 2,
+        Both = 3
+    }
+}
