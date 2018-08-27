fd8134452472150938fec9837fc04035f37dd118
shuao23 <shuao23@gmail.com>
2018-08-27 11:19:54 -1000

    moved stats from move to motor

--- Assets/Scripts/Motor/JumpMotor.cs
+++ Assets/Scripts/Motor/JumpMotor.cs
@@ -5,8 +5,7 @@ public class JumpMotor
 {
     private Rigidbody2D rigidbody;
 
-
-    public float Velocity { get; set; }
+    public Stats MotorStats { get; private set; }
 
 
     public JumpMotor(Rigidbody2D rigidbody)
@@ -23,5 +22,18 @@ public class JumpMotor
     public void Update(float deltaTime)
     {
         rigidbody.AddForce(Vector2.up * Velocity, ForceMode2D.Impulse);
+    }
+
+
+    [Serializable]
+    public class Stats
+    {
+        [SerializeField]
+        private float velocity;
+
+        public float Velocity {
+            get { return velocity; }
+            set { velocity = value; }
+        }
     }
 }
--- Assets/Scripts/Move/JumpMove.cs
+++ Assets/Scripts/Move/JumpMove.cs
@@ -4,21 +4,14 @@ using UnityEngine;
 public class JumpMove : Move
 {
     public JumpMotor Motor { get; private set; }
-    public MoveStats Stats { get; private set; }
 
-    public JumpMove(string name, MoveStats stats, JumpMotor motor) : base(name)
+    public JumpMove(string name, JumpMotor motor) : base(name)
     {
-        if (stats == null)
-        {
-            throw new ArgumentNullException("stats");
-        }
-
         if (motor == null)
         {
             throw new ArgumentNullException("motor");
         }
 
-        Stats = stats;
         Motor = motor;
     }
 
@@ -26,18 +19,5 @@ public class JumpMove : Move
     {
         Motor.Update(deltaTime);
         Close();
-    }
-
-
-    [Serializable]
-    public class MoveStats
-    {
-        [SerializeField]
-        private float velocity;
-
-        public float Velocity {
-            get { return velocity; }
-            set { velocity = value; }
-        }
     }
 }
