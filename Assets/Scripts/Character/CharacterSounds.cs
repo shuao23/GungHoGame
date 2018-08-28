a393468a7997adc74c8f278ba2a70d4ff7e894f4
shuao23 <shuao23@gmail.com>
2018-08-28 10:35:22 -1000

    Addedd animtion events for character sounds

---
+++ Assets/Scripts/Character/CharacterSounds.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSounds : MonoBehaviour {
+
+	private void OnFootStep()
+    {
+        Debug.Log("On foot step");
+    }
+}
