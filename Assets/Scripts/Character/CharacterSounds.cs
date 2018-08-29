a8474476720c59f8ea4b09889236f8f736e9315d
shuao23 <shuao23@gmail.com>
2018-08-28 15:33:12 -1000

    Added soundmixer and more sounds for robot sound fx

--- Assets/Scripts/Character/CharacterSounds.cs
+++ Assets/Scripts/Character/CharacterSounds.cs
@@ -5,8 +5,56 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class CharacterSounds : MonoBehaviour {
 
-	private void OnFootStep()
-    {
+    [SerializeField]
+    private AudioSource audioSource;
+    [SerializeField]
+    private AudioClip footStep;
+    [SerializeField]
+    private AudioClip jump;
+    [SerializeField]
+    private AudioClip land;
+    [SerializeField]
+    private AudioClip damaged;
+    [SerializeField]
+    private AudioClip death;
+    [SerializeField]
+    private AudioClip punch;
+    [SerializeField]
+    private AudioClip rocketPunch;
 
+
+    private void OnFootStep()
+    {
+        audioSource.PlayOneShot(footStep);
+    }
+
+    private void OnJump()
+    {
+        audioSource.PlayOneShot(jump);
+    }
+
+    private void OnLand()
+    {
+        audioSource.PlayOneShot(land);
+    }
+
+    private void OnPunch()
+    {
+        audioSource.PlayOneShot(punch);
+    }
+
+    private void OnRocketPunch()
+    {
+        audioSource.PlayOneShot(rocketPunch);
+    }
+
+    private void OnDamage()
+    {
+        audioSource.PlayOneShot(damaged);
+    }
+
+    private void OnDeath()
+    {
+        audioSource.PlayOneShot(death);
     }
 }
