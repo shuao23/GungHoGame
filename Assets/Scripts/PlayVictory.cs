0bbeda58b36effc500801f2ac0aa8db2a4e442ba
shuao23 <shuao23@gmail.com>
2018-08-28 17:39:32 -1000

    Made the main menu better
    Added victory sound

---
+++ Assets/Scripts/PlayVictory.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVictory : MonoBehaviour {
+
+    [SerializeField]
+    private AudioSource audioSource;
+    [SerializeField]
+    private AudioClip victory;
+
+    private void OnVictory()
+    {
+        audioSource.PlayOneShot(victory);
+    }
+}
