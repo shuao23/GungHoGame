07adcd75ed0dfdd2a9d6840cacf9910714a7169f
shuao23 <shuao23@gmail.com>
2018-08-28 16:27:15 -1000

    Added enemy ball, created prefab, and also placed enemy in backdrop

---
+++ Assets/Scripts/SimpleRotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SimpleRotate : MonoBehaviour {
+
+    [SerializeField]
+    private Vector3  rotateSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(rotateSpeed * Time.deltaTime);
+	}
+}
