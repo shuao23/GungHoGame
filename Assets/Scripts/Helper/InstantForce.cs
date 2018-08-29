40117b962fab68cd32657631fcedb146664b0ace
shuao23 <shuao23@gmail.com>
2018-08-28 21:06:41 -1000

    Added rocket effects

---
+++ Assets/Scripts/Helper/InstantForce.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class InstantForce : MonoBehaviour {
+
+    [SerializeField]
+    private Vector3 force;
+
+    private new Rigidbody rigidbody;
+
+    private Rigidbody Rigidbody {
+        get {
+            if(rigidbody == null)
+            {
+                rigidbody = GetComponent<Rigidbody>();
+            }
+            return rigidbody;
+        }
+    }
+
+	void Start () {
+        Rigidbody.AddForce(force, ForceMode.Impulse);
+	}
+
+}
